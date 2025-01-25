4e1a97f63e8496db65187ac00e03faebb8e07483
CarrotNinja <r-sherwani@outlook.com>
2025-01-24 22:49:11 -0500

    push :)

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -14,10 +14,13 @@ public class MeepMeepTesting {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(10.53, -68.65, Math.toRadians(90.00)))
-                        .splineToConstantHeading(new Vector2d(9.59, -33.44), Math.toRadians(91.52))
-                        .lineTo(new Vector2d(46.85, -67.34))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-2.14, -56.72, Math.toRadians(90.00)))
+                        .lineTo(new Vector2d(-46.48, 4.38))
+                        .splineTo(new Vector2d(-17.42, 39.40), Math.toRadians(50.31))
+                        .splineTo(new Vector2d(28.78, 43.50), Math.toRadians(5.07))
                         .build());
+
+
 
 
 
